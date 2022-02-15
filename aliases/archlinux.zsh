b683bdb2079f79cd8fd6e1b6a89628c48e71f89a
jprtal <j.portal@outlook.com>
2022-02-15 12:16:45 +0100

    Fix paru news

--- aliases/archlinux.zsh
+++ aliases/archlinux.zsh
@@ -2,7 +2,7 @@
 alias yay="paru"
 alias yeet="paru -Rcs"
 alias whee="paru -Sccd"
-alias update="paru -Syu --news; echo; flatpak update"
+alias update="paru -Syu --newsonupgrade; echo; flatpak update"
 
 # Mirrors
 alias update-mirrors="sudo reflector --country France,Germany --age 4 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
