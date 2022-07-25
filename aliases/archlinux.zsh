ff89233500d622cec7268b8df90ed0dc5c53c556
jprtal <j.portal@outlook.com>
2022-07-25 19:29:10 +0200

    Add alias to locate .pac* files

--- aliases/archlinux.zsh
+++ aliases/archlinux.zsh
@@ -6,3 +6,5 @@ alias update="paru -Syu --newsonupgrade; echo; flatpak update"
 
 # Mirrors
 alias update-mirrors="sudo reflector --country France,Germany --age 4 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
+
+alias pacfiles="find / -regextype posix-extended -regex '.+\.pac(new|save)' 2> /dev/null"
