943915858910905e7cb9a1359ba22c0f210b6e21
jprtal <j.portal@outlook.com>
2022-07-25 22:13:03 +0200

    Move dotfiles git dir

--- aliases/management.zsh
+++ aliases/management.zsh
@@ -1,2 +1,2 @@
 # https://wiki.archlinux.org/index.php/Dotfiles#Tracking_dotfiles_directly_with_Git
-alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dotfiles='git --git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/ --work-tree=$HOME'
