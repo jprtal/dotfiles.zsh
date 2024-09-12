efa7646330ed87c5e4370c1cc6a243e67fe658da
jprtal <j@prtal.io>
2024-09-12 21:40:13 +0200

    Upgrade function

--- aliases/archlinux.zsh
+++ aliases/archlinux.zsh
@@ -2,7 +2,6 @@
 alias yay="paru"
 alias yeet="paru -Rnsc"
 alias whee="paru -Sccd"
-alias update="paru -Syu --newsonupgrade; echo; flatpak update"
 
 # Mirrors
 alias update-mirrors="sudo reflector --country France,Germany --age 4 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
