822785cbd8ace54968101707ed66038a89434cb0
jprtal <j@prtal.io>
2026-02-11 19:19:34 +0100

    Add dotfiles.zsh alias

--- aliases/management.zsh
+++ aliases/management.zsh
@@ -1,2 +1,4 @@
 # https://wiki.archlinux.org/index.php/Dotfiles#Tracking_dotfiles_directly_with_Git
 alias dotfiles='git --git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/ --work-tree=$HOME'
+
+alias dotfiles.zsh='git --git-dir=${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles/modules/.config/zsh'
